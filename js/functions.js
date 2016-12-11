6889147f8a95e6f4184a23daf38165a281ad4572
Dimitris Karapanos <karapanos.dimitris@gmail.com>
2016-12-11 19:20:06 +0100

    Contact section progress

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,11 @@ var window_height = $(window).height();
 var navbar_height = $('nav').outerHeight() + 50;
 
 $( document ).ready(function() {
+    
     fixNavbar();
     fixClients();
+    initMap();
+    
     $(window).scroll(function(){
         fixNavbar();
     });
@@ -172,6 +175,28 @@ function fixNavbar() {
             $('nav[role="navigation"]').addClass('navbar-transparent');
         }
     }
+}
+
+function initMap(){
+    var myLatlng = new google.maps.LatLng(48.7938094, 2.3553952);
+    
+    var mapOptions = {
+      zoom: 14,
+      center: myLatlng,
+      scrollwheel: false,
+      disableDefaultUI: true,
+      styles: [{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"on"},{"gamma":"1.82"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"gamma":"1.96"},{"lightness":"-9"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"lightness":"25"},{"gamma":"1.00"},{"saturation":"-100"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffaa00"},{"saturation":"-43"},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"},{"hue":"#ffaa00"},{"saturation":"-70"}]},{"featureType":"road.highway.controlled_access","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"-100"},{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"saturation":"-100"},{"lightness":"40"},{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"gamma":"0.80"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"off"}]}]
+    };
+  
+    var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+    
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"4 Rue René Balayn, 94800 Villejuif, France"
+    });
+    
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
 }
 
 (function( $ ){
